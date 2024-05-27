9a8b2b7eb79152ecee647ec10faec56ecaf9c6b0
Gréczi-Zsoldos Miklós <mercenary.michael@mailbox.unideb.hu>
2024-05-27 18:24:32 +0200

    build: Created Square and LabyrinthModel class

---
+++ src/main/java/LabyrinthModel.java
@@ -0,0 +1,6 @@
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+
+public class LabyrinthModel {
+    
+}
---
+++ src/main/java/Square.java
@@ -0,0 +1,5 @@
+public enum Square {
+    NONE,
+    PLAYER,
+    ENEMY;
+}
