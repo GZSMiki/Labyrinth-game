b98a7a98cf7e1b7ee5a10b59b132da98e9842836
Gréczi-Zsoldos Miklós <mercenary.michael@mailbox.unideb.hu>
2024-06-12 16:09:21 +0200

    build: completed isMoveBlocked, makeMove, getLegalMoves methods and added helper methods

--- src/main/java/LabyrinthModel.java
+++ src/main/java/LabyrinthModel.java
@@ -2,9 +2,7 @@ import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import puzzle.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class LabyrinthModel implements State<Position>{
     public static final int BOARD_SIZE = 6;
@@ -15,8 +13,13 @@ public class LabyrinthModel implements State<Position>{
 
     private Position Player;
     private Position Enemy;
+    private Square turn;
+
+    private Direction direction;
     public LabyrinthModel() {
         board = new ReadOnlyObjectWrapper[BOARD_SIZE][BOARD_SIZE];
+        turn = Square.PLAYER;
+
         for (var i = 0; i < BOARD_SIZE; i++) {
             for (var j = 0; j < BOARD_SIZE; j++) {
                 if (i == 0 && j == 0)  {
@@ -59,32 +62,23 @@ public class LabyrinthModel implements State<Position>{
     }
 
 
-    public String toString() {
-        var sb = new StringBuilder();
-        for (var i = 0; i < BOARD_SIZE; i++) {
-            for (var j = 0; j < BOARD_SIZE; j++) {
-                sb.append(board[i][j].get().ordinal()).append(' ');
-            }
-            sb.append('\n');
-        }
-        return sb.toString();
-    }
-
     public static void main(String[] args) {
         var model = new LabyrinthModel();
         System.out.println(model);
-        model.makeMove(new TwoPhaseMove<>(new Position(0, 0), new Position(1, 0)));
+        model.makeMove(new Position(0,1));
         System.out.println(model);
-        model.makeMove(new TwoPhaseMove<>(new Position(0, 0), new Position(0, 1)));
-        System.out.println(model);
-        model.makeMove(new TwoPhaseMove<>(new Position(0, 1), new Position(0, 2)));
-        System.out.println(model);
-        model.makeMove(new TwoPhaseMove<>(new Position(0, 2), new Position(0, 3)));
-        System.out.println(model);
-        model.makeMove(new TwoPhaseMove<>(new Position(0, 3), new Position(0, 4)));
+        model.makeMove(new Position(2, 3));
         System.out.println(model);
     }
 
+    private Position getCurrentTurnPosition(Square turn) {
+        if(turn == Square.PLAYER) return Player;
+        return Enemy;
+    }
+
+    private void nextTurn() {
+        turn = turn.equals(Square.PLAYER) ? Square.ENEMY : Square.PLAYER;
+    }
     @Override
     public boolean isSolved() {
         return false;
@@ -92,65 +86,84 @@ public class LabyrinthModel implements State<Position>{
 
     @Override
     public boolean isLegalMove(Position position) {
+        if(isOnBoard(position)) {
+            if(!isMoveBlocked(position) && isMoveDistanceOne(position)){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public void makeMove(Position position) {
-
+    public void makeMove(Position pos) {
+        Position currentPos = getCurrentTurnPosition(turn);
+        if(isLegalMove(pos)) {
+            board[pos.row()][pos.col()].set(turn);
+            board[currentPos.row()][currentPos.col()].set(Square.NONE);
+            nextTurn();
+            //TODO: make ai for the enemy, then call it here, if the turn is Enemy
+        }
     }
 
     @Override
     public Set<Position> getLegalMoves() {
+        Position currentPos = getCurrentTurnPosition(turn);
+        var legalMoves = new HashSet<Position>();
+        Position up = new Position(currentPos.row()+1, currentPos.col());
+        Position down = new Position(currentPos.row()-1, currentPos.col());
+        Position left = new Position(currentPos.row(), currentPos.col()-1);
+        Position right = new Position(currentPos.row(), currentPos.col()+1);
+
+        if(isLegalMove(up)) {
+            legalMoves.add(up);
+        }
+        if(isLegalMove(down)) {
+            legalMoves.add(down);
+        }
+        if(isLegalMove(left)) {
+            legalMoves.add(left);
+        }
+        if(isLegalMove(right)) {
+            legalMoves.add(right);
+        }
+        return legalMoves;
+    }
+
+    @Override
+    public State<Position> clone() {
         return null;
     }
 
-    /*
-    @Override
-    public boolean isLegalMove(TwoPhaseMove<Position> positions) {
-        return isLegalToMoveFrom(positions.from()) &&
-                !isMoveBlocked(positions);
-    }
+    public boolean isMoveBlocked(Position position) {
+        Position currentPos = getCurrentTurnPosition(turn);
+        direction = Direction.of(position.row() - currentPos.row(),
+                position.col() - currentPos.col());
 
-    public boolean isMoveBlocked(TwoPhaseMove<Position> positions) {
-        Position from = positions.from();
-        Position to = positions.to();
-        if(verticalWalls.contains(from) && (to.col() - from.col() == 1)) {
+        if(verticalWalls.contains(currentPos) && direction == Direction.RIGHT) {
             return true;
         }
-        if(verticalWalls.contains(to) && (to.col() - from.col() == -1)) {
+        if(verticalWalls.contains(position) && direction == Direction.LEFT) {
             return true;
         }
-        if(horizontalWalls.contains(from) && (to.row() - from.col() == 1)) {
+        if(horizontalWalls.contains(currentPos) && direction == Direction.DOWN) {
             return true;
         }
-        if(horizontalWalls.contains(to) && (to.col() - from.col() == -1)) {
+        if(horizontalWalls.contains(position) && direction == Direction.UP) {
             return true;
         }
 
         return false;
     }
 
-    @Override
-    public void makeMove(TwoPhaseMove<Position> pos) {
-        if(isLegalMove(pos)) {
-            board[pos.to().row()][pos.to().col()]
-                    .set(board[pos.from().row()][pos.from().col()].get());
-            board[pos.from().row()][pos.from().col()].set(Square.NONE);
+    private boolean isMoveDistanceOne(Position position) {
+        Position currentPos = getCurrentTurnPosition(turn);
+        int rowDistance = position.row() - currentPos.row();
+        int colDistance = position.col() - currentPos.col();
+        if(Math.abs(rowDistance) > 1 || Math.abs(colDistance) > 1) {
+            return false;
         }
+        return true;
     }
-
-    @Override
-    public Set<TwoPhaseMove<Position>> getLegalMoves() {
-        return null;
-    }
-
-     */
-
-    private boolean isPlayer(Position position) {
-        return getSquare(position) == Square.PLAYER;
-    }
-
     private Square getSquare(Position pos) {
         int row = pos.row();
         int col = pos.col();
@@ -165,8 +178,14 @@ public class LabyrinthModel implements State<Position>{
         return true;
     }
 
-    @Override
-    public TwoPhaseMoveState<Position> clone() {
-        return null;
+    public String toString() {
+        var sb = new StringBuilder();
+        for (var i = 0; i < BOARD_SIZE; i++) {
+            for (var j = 0; j < BOARD_SIZE; j++) {
+                sb.append(board[i][j].get().ordinal()).append(' ');
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
     }
 }
